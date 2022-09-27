9b8b9001f9bb5f03b92fda6588645a4f69ea3424
Taiwan, RA Digital RD8 <twradigitalrd8@deloitte.com.tw>
2022-09-27 18:39:50 +0800

    fix(BugFix): 00000
    00000

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,17 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ChildContentComponent } from './child-content/child-content.component';
+import { ChildFormComponent } from './child-form/child-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChildContentComponent
+    ChildFormComponent,
   ],
   imports: [
-    AppRoutingModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
